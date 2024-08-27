493271a1f254137c9d84cc60f47d06f522d6e7c4
ElongJacquie <jacquie.duditlieux-elong@stud.thga.de>
2024-08-27 02:11:35 +0200

    update

---
+++ A0/A07/A07_1.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <getopt.h>
+#include <time.h>
+
+void print_usage();
+int main(int argc, char *argv[])
+{
+    int opt;
+    char* date_str = NULL;
+    struct tm date_tm = {0};
+    time_t date_time;
+    char weekday[10];
+
+    while((opt = getopt(argc,argv,"t:")) != -1){
+        switch(opt){
+      case 't':
+          date_str = optarg;
+          break;
+      default:
+          print_usage();
+          exit(EXIT_FAILURE);
+        }
+    }
+
+    if(date_str == NULL){
+        print_usage();
+        exit(EXIT_FAILURE);
+    }
+
+    struct tm min_tm1 = {0};
+    struct tm min_tm2 = {0};
+
+    strptime("2024-01-01","%Y-%m-%d",&min_tm1);
+    if(difftime(mktime(&date_tm),mktime(&min_tm1)) < 0){
+        fprint(stderr,"Date must be after 2024-01-01.\n");
+         exit(EXIT_FAILURE);
+    }
+
+
+
+    date_time = mktime(&date_tm);
+    strftime(weekday,sizeof(weekday),"%A",&date_tm);
+
+    printf("The day of the week for %s is %s.\n",date_str,weekday);
+
+    return 0;
+}
+
+void print_usage(){
+   printf("Usage: ./weekday -t YYYY-MM-DD\n");
+}
+
